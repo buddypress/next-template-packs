a3c6f7715a08447afec7dd4d5d1d85ac9944b6d1
Hugo <hugo.ashmore@gmail.com>
2016-04-12 15:24:07 +0100

    Updates dir-search-form.php for template pack specifics: * Change markup to li from div * Remove ID * Remove str_rep on input id * add component specific name as class token to replace ID.

--- bp-templates/bp-next/buddypress/common/dir-search-form.php
+++ bp-templates/bp-next/buddypress/common/dir-search-form.php
@@ -1,8 +1,8 @@
-<div id="<?php esc_attr_e( bp_current_component() ); ?>-dir-search" class="dir-search" role="search">
+<li class="dir-search <?php esc_attr_e( bp_current_component() ); ?>-dir-search" role="search" data-bp-search="<?php esc_attr_e( bp_current_component() ); ?>">
 	<form action="" method="get" id="search-<?php  esc_attr_e( bp_current_component() ); ?>-form">
 		<label for="<?php bp_search_input_name(); ?>" class="bp-screen-reader-text"><?php bp_search_placeholder(); ?></label>
 			<input type="text" name="<?php esc_attr_e( bp_core_get_component_search_query_arg() ); ?>" id="<?php bp_search_input_name(); ?>" placeholder="<?php bp_search_placeholder(); ?>" />
-		<input type="submit" id="<?php esc_attr_e(str_replace('_', '-', bp_get_search_input_name() ) ); ?>-submit" name="<?php bp_search_input_name(); ?>_submit" value="<?php esc_html_e( 'Search', 'buddypress' ); ?>" />
+		<input type="submit" id="<?php esc_attr_e( bp_get_search_input_name() ); ?>-submit" name="<?php bp_search_input_name(); ?>_submit" value="<?php esc_html_e( 'Search', 'buddypress' ); ?>" />
 	</form>
-</div><!-- #<?php esc_attr_e( bp_current_component() ); ?>-dir-search -->
+</li><!-- #<?php esc_attr_e( bp_current_component() ); ?>-dir-search -->
 
