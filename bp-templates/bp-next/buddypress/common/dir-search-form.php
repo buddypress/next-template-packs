1923f711c6570ace4fb06bbf4f055ee15effa0c2
Hugo <hugo.ashmore@gmail.com>
2016-04-13 21:02:04 +0100

    Change the hyphen to underscore to revert back fully to match name attr :(

--- bp-templates/bp-next/buddypress/common/dir-search-form.php
+++ bp-templates/bp-next/buddypress/common/dir-search-form.php
@@ -2,7 +2,7 @@
 	<form action="" method="get" id="search-<?php  esc_attr_e( bp_current_component() ); ?>-form">
 		<label for="<?php bp_search_input_name(); ?>" class="bp-screen-reader-text"><?php bp_search_placeholder(); ?></label>
 			<input type="text" name="<?php esc_attr_e( bp_core_get_component_search_query_arg() ); ?>" id="<?php bp_search_input_name(); ?>" placeholder="<?php bp_search_placeholder(); ?>" />
-		<input type="submit" id="<?php esc_attr_e( bp_get_search_input_name() ); ?>-submit" name="<?php bp_search_input_name(); ?>_submit" value="<?php esc_html_e( 'Search', 'buddypress' ); ?>" />
+			<input type="submit" id="<?php esc_attr_e( bp_get_search_input_name() ); ?>_submit" name="<?php bp_search_input_name(); ?>_submit" value="<?php esc_html_e( 'Search', 'buddypress' ); ?>" />
 	</form>
 </li><!-- #<?php esc_attr_e( bp_current_component() ); ?>-dir-search -->
 
