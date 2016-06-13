ff0ff3ad873f96839be2827bef831c87ba029171
imath <math.viet@gmail.com>
2016-06-13 21:01:54 +0200

    Add the missing directory-filters.php template
    See #24

---
+++ bp-templates/bp-nouveau/buddypress/common/filters/directory-filters.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * BP Nouveau Component's directory filters template.
+ *
+ * @since 1.0.0
+ *
+ * @package BP Nouveau
+ */
+?>
+
+<ul id="dir-filters" class="dir-component-filters clearfix">
+	<li id="<?php bp_nouveau_directory_filter_container_id(); ?>" class="last filter">
+		<label for="<?php bp_nouveau_directory_filter_id(); ?>">
+			<span class="bp-screen-reader-text"><?php bp_nouveau_directory_filter_label(); ?></span>
+		</label>
+		<select id="<?php bp_nouveau_directory_filter_id(); ?>" data-bp-filter="<?php bp_nouveau_directory_filter_component(); ?>">
+
+			<?php bp_nouveau_directory_filter_options(); ?>
+
+		</select>
+	</li>
+</ul>
