879da7263d4b3f474530048c97e8c82b3995c6ee
imath <math.viet@gmail.com>
2016-06-17 19:07:30 +0200

    Create a template hierarchy for each template part of the single members items
    The template hierarchy is the same than for the user front.php (User ID, User Nicename & Member type). See #32
    Carry on the do_action hunt by removing 2 of theme from the members/single/home.php to wrap them into the new function bp_nouveau_member_template_part(). See #6

--- bp-templates/bp-nouveau/buddypress/members/single/home.php
+++ bp-templates/bp-nouveau/buddypress/members/single/home.php
@@ -42,54 +42,7 @@
 
 	<div id="item-body">
 
-		<?php
-
-		/**
-		 * Fires before the display of member body content.
-		 *
-		 * @since 1.2.0
-		 */
-		do_action( 'bp_before_member_body' );
-
-		if ( bp_is_user_front() ) :
-			bp_displayed_user_front_template_part();
-
-		elseif ( bp_is_user_activity() ) :
-			bp_get_template_part( 'members/single/activity' );
-
-		elseif ( bp_is_user_blogs() ) :
-			bp_get_template_part( 'members/single/blogs'    );
-
-		elseif ( bp_is_user_friends() ) :
-			bp_get_template_part( 'members/single/friends'  );
-
-		elseif ( bp_is_user_groups() ) :
-			bp_get_template_part( 'members/single/groups'   );
-
-		elseif ( bp_is_user_messages() ) :
-			bp_get_template_part( 'members/single/messages' );
-
-		elseif ( bp_is_user_profile() ) :
-			bp_get_template_part( 'members/single/profile'  );
-
-		elseif ( bp_is_user_notifications() ) :
-			bp_get_template_part( 'members/single/notifications' );
-
-		elseif ( bp_is_user_settings() ) :
-			bp_get_template_part( 'members/single/settings' );
-
-		// If nothing sticks, load a generic template
-		else :
-			bp_get_template_part( 'members/single/plugins'  );
-
-		endif;
-
-		/**
-		 * Fires after the display of member body content.
-		 *
-		 * @since 1.2.0
-		 */
-		do_action( 'bp_after_member_body' ); ?>
+		<?php bp_nouveau_member_template_part();?>
 
 	</div><!-- #item-body -->
 
