buddypress/next-template-packs

commit ed58615003d33759c476edbfc245e3adb20c3331

Introduce a new template tag to replace the "bp_member_header_actions" do_action hook.
bp_nouveau_member_header_buttons() is using the BP_Buttons_Group class to manage the action buttons inside the users profile headers. It also wraps the "bp_member_header_actions" but displays a warning when used to inform users they can now use a filter to choose the position where they want to output their buttons in this group of buttons.

See #6

bp-templates/bp-nouveau/includes/friends/loader.php
@@ -57,10 +57,6 @@ class BP_Nouveau_Friends {
 	 * @since 1.0.0
 	 */
 	private function setup_actions() {
-		if ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
-			add_action( 'bp_member_header_actions', 'bp_add_friend_button', 5 );
-		}
-
 		$ajax_actions = array(
 			array( 'friends_remove_friend'       => array( 'function' => 'bp_nouveau_ajax_addremove_friend', 'nopriv' => false ) ),
 			array( 'friends_add_friend'          => array( 'function' => 'bp_nouveau_ajax_addremove_friend', 'nopriv' => false ) ),

bp-templates/bp-nouveau/includes/functions.php
@@ -281,6 +281,11 @@ function bp_nouveau_get_forsaken_hooks() {
 			'message_type' => 'warning',
 			'message'      => __( 'The &#39;bp_activity_entry_meta&#39; action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the &#39;bp_nouveau_get_activity_entry_buttons&#39; filter instead', 'bp-nouveau' ),
 		),
+		'bp_member_header_actions' => array(
+			'hook_type'    => 'action',
+			'message_type' => 'warning',
+			'message'      => __( 'The &#39;bp_member_header_actions&#39; action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the &#39;bp_nouveau_get_member_header_buttons&#39; filter instead', 'bp-nouveau' ),
+		),
 	);
 }
 

bp-templates/bp-nouveau/includes/activity/loader.php
@@ -59,12 +59,6 @@ class BP_Nouveau_Activity {
 	 * @since 1.0.0
 	 */
 	private function setup_actions() {
-		if ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
-			if ( bp_activity_do_mentions() ) {
-				add_action( 'bp_member_header_actions', 'bp_send_public_message_button',  20 );
-			}
-		}
-
 		$ajax_actions = array(
 			array( 'activity_filter'                 => array( 'function' => 'bp_nouveau_ajax_object_template_loader',      'nopriv' => true ) ),
 			array( 'get_single_activity_content'     => array( 'function' => 'bp_nouveau_ajax_get_single_activity_content', 'nopriv' => true ) ),

bp-templates/bp-nouveau/includes/members/functions.php
@@ -119,3 +119,22 @@ function bp_nouveau_get_members_filters( $context = '' ) {
 
 	return bp_nouveau_parse_hooked_options( 'bp_members_directory_order_options', $filters );
 }
+
+/**
+ * Catch the arguments for buttons
+ *
+ * @since 1.0.0
+ *
+ * @param  array $buttons The arguments of the button that BuddyPress is about to create.
+ * @return array An empty array to stop the button creation process.
+ */
+function bp_nouveau_members_catch_button_args( $button = array() ) {
+	/**
+	 * Globalize the arguments so that we can use it
+	 * in bp_nouveau_get_member_header_buttons().
+	 */
+	bp_nouveau()->members->button_args = $button;
+
+	// return an empty array to stop the button creation process
+	return array();
+}

bp-templates/bp-nouveau/includes/messages/loader.php
@@ -59,10 +59,6 @@ class BP_Nouveau_Messages {
 	 * @since 1.0.0
 	 */
 	private function setup_actions() {
-		if ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
-			add_action( 'bp_member_header_actions', 'bp_send_private_message_button', 20 );
-		}
-
 		// Notices
 		add_action( 'widgets_init',     'bp_nouveau_unregister_notices_widget'       );
 		add_action( 'template_notices', 'bp_nouveau_sitewide_notices',          9999 );

bp-templates/bp-nouveau/buddypress/members/single/cover-image-header.php
@@ -35,14 +35,7 @@ do_action( 'bp_before_member_header' ); ?>
 				<h2 class="user-nicename">@<?php bp_displayed_user_mentionname(); ?></h2>
 			<?php endif; ?>
 
-			<div id="item-buttons"><?php
-
-				/**
-				 * Fires in the member header actions section.
-				 *
-				 * @since 1.2.6
-				 */
-				do_action( 'bp_member_header_actions' ); ?></div><!-- #item-buttons -->
+			<div id="item-buttons"><?php bp_nouveau_member_header_buttons(); ?></div><!-- #item-buttons -->
 
 			<span class="activity"><?php bp_last_activity( bp_displayed_user_id() ); ?></span>
 

bp-templates/bp-nouveau/buddypress/members/single/member-header.php
@@ -56,14 +56,7 @@ do_action( 'bp_before_member_header' ); ?>
 
 		<div id="item-buttons">
 
-			<?php
-
-			/**
-			 * Fires in the member header actions section.
-			 *
-			 * @since 1.2.6
-			 */
-			do_action( 'bp_member_header_actions' ); ?>
+			<?php bp_nouveau_member_header_buttons(); ?>
 
 		</div><!-- #item-buttons -->
 

bp-templates/bp-nouveau/includes/classes.php
@@ -80,7 +80,7 @@ class BP_Buttons_Group {
 
 			// Set the wrapper to default value if a class or an id for it is defined.
 			if ( ( ! empty( $r['wrapper_class'] ) || ! empty( $r['wrapper_id'] ) ) && false === $r['wrapper'] ) {
-				$r['wrapper'] = '';
+				$r['wrapper'] = 'div';
 			}
 
 			$this->group[ $r['id'] ] = $r;

commit 823fe674e427f0ac02d09d7a5d84b07c81a6bd09

Fix a regression with ajax introduced in ed58615003d33759c476edbfc245e3adb20c3331
As i am using one id for the friendship button i need to recreate the data action attribute, too bad BuddyPress naming is not consistent!

See #6

bp-templates/bp-nouveau/buddypress/members/single/home.php
@@ -19,7 +19,7 @@
 	 */
 	do_action( 'bp_before_member_home_content' ); ?>
 
-	<div id="item-header" role="complementary" data-bp-item-id="<?php echo bp_displayed_user_id(); ?>" data-bp-item-type="members">
+	<div id="item-header" role="complementary" data-bp-item-id="<?php echo bp_displayed_user_id(); ?>" data-bp-item-component="members">
 
 		<?php
 		/**

bp-templates/bp-nouveau/includes/friends/loader.php
@@ -92,6 +92,7 @@ class BP_Nouveau_Friends {
 			'friends_awaiting_response',
 			'friends_is_friend',
 			'friends_not_friends',
+			'friends_member_profile_friendship',
 		);
 
 		foreach ( $buttons as $button ) {

bp-templates/bp-nouveau/includes/functions.php
@@ -15,8 +15,31 @@ function bp_nouveau_ajax_button( $output ='', $button = null, $before ='', $afte
 		return $output;
 	}
 
+	$data_attribute = $button->id;
+
+	if ( 'member_profile_friendship' === $button->id ) {
+		$parse_class = explode( ' ', $button->link_class );
+
+		if ( false !== $parse_class ) {
+			$find_id = array_intersect( $parse_class, array(
+				'pending_friend',
+				'awaiting_response_friend',
+				'is_friend',
+				'not_friends',
+			) );
+
+			if ( 1 === count( $find_id ) ) {
+				$data_attribute = reset( $find_id );
+
+				if ( in_array( $data_attribute, array( 'pending_friend', 'awaiting_response_friend' ) ) ) {
+					$data_attribute = str_replace( '_friend', '', $data_attribute );
+				}
+			}
+		}
+	}
+
 	// Add span bp-screen-reader-text class
-	return $before . '<a'. $button->link_href . $button->link_title . $button->link_id . $button->link_rel . $button->link_class . ' data-bp-btn-action="' . $button->id . '">' . $button->link_text . '</a>' . $after;
+	return $before . '<a'. $button->link_href . $button->link_title . $button->link_id . $button->link_rel . $button->link_class . ' data-bp-btn-action="' . $data_attribute . '">' . $button->link_text . '</a>' . $after;
 }
 
 if ( ! class_exists( 'BP_Nouveau_Object_Nav_Widget' ) ) :

bp-templates/bp-nouveau/js/buddypress.js
@@ -743,7 +743,7 @@ window.bp = window.bp || {};
 						$( self.objectNavParent + ' [data-bp-scope="personal"] span' ).html( personal_count );
 					}
 
-					target.replaceWith( response.data.contents );
+					target.parent().replaceWith( response.data.contents );
 				}
 			} );
 		}