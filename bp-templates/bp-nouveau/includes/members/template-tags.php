buddypress/next-template-packs

commit ed58615003d33759c476edbfc245e3adb20c3331

Introduce a new template tag to replace the "bp_member_header_actions" do_action hook.
bp_nouveau_member_header_buttons() is using the BP_Buttons_Group class to manage the action buttons inside the users profile headers. It also wraps the "bp_member_header_actions" but displays a warning when used to inform users they can now use a filter to choose the position where they want to output their buttons in this group of buttons.

See #6

bp-templates/bp-nouveau/includes/members/template-tags.php
@@ -81,3 +81,130 @@ function bp_nouveau_after_members_directory_content() {
 	 */
 	do_action( 'bp_after_directory_members_page' );
 }
+
+/**
+ * Output the action buttons for the displayed user profile
+ *
+ * @since 1.0.0
+ */
+function bp_nouveau_member_header_buttons() {
+	echo join( ' ', bp_nouveau_get_member_header_buttons() );
+
+	/**
+	 * Fires in the member header actions section.
+	 *
+	 * @since 1.2.6 (BuddyPress)
+	 */
+	do_action( 'bp_member_header_actions' );
+}
+
+	/**
+	 * Get the action buttons for the displayed user profile
+	 *
+	 * @since 1.0.0
+	 */
+	function bp_nouveau_get_member_header_buttons() {
+		// Not really sure why BP Legacy needed to do this...
+		if ( is_admin() && ! ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+			return;
+		}
+
+		$buttons = array();
+		$user_id = bp_displayed_user_id();
+
+		if ( ! $user_id ) {
+			return $buttons;
+		}
+
+		if ( bp_is_active( 'friends' ) ) {
+			/**
+			 * This filter workaround is waiting for a core adaptation
+			 * so that we can directly get the friends button arguments
+			 * instead of the button.
+			 * @see https://buddypress.trac.wordpress.org/ticket/7126
+			 */
+			add_filter( 'bp_get_add_friend_button', 'bp_nouveau_members_catch_button_args', 100, 1 );
+
+			bp_get_add_friend_button();
+
+			remove_filter( 'bp_get_add_friend_button', 'bp_nouveau_members_catch_button_args', 100, 1 );
+
+			if ( ! empty( bp_nouveau()->members->button_args ) ) {
+				$buttons['member_profile_friendship'] = wp_parse_args( array(
+					'id'       => 'member_profile_friendship',
+					'position' => 5,
+				), bp_nouveau()->members->button_args );
+
+				unset( bp_nouveau()->members->button_args );
+			}
+		}
+
+		if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() ) {
+			/**
+			 * This filter workaround is waiting for a core adaptation
+			 * so that we can directly get the public message button arguments
+			 * instead of the button.
+			 * @see https://buddypress.trac.wordpress.org/ticket/7126
+			 */
+			add_filter( 'bp_get_send_public_message_button', 'bp_nouveau_members_catch_button_args', 100, 1 );
+
+			bp_get_send_public_message_button();
+
+			remove_filter( 'bp_get_send_public_message_button', 'bp_nouveau_members_catch_button_args', 100, 1 );
+
+			if ( ! empty( bp_nouveau()->members->button_args ) ) {
+				$buttons['public_message'] = wp_parse_args( array(
+					'position' => 15,
+				), bp_nouveau()->members->button_args );
+
+				unset( bp_nouveau()->members->button_args );
+			}
+		}
+
+		if ( bp_is_active( 'messages' ) ) {
+			/**
+			 * This filter workaround is waiting for a core adaptation
+			 * so that we can directly get the private messages button arguments
+			 * instead of the button.
+			 * @see https://buddypress.trac.wordpress.org/ticket/7126
+			 */
+			add_filter( 'bp_get_send_message_button_args', 'bp_nouveau_members_catch_button_args', 100, 1 );
+
+			bp_get_send_message_button();
+
+			remove_filter( 'bp_get_send_message_button_args', 'bp_nouveau_members_catch_button_args', 100, 1 );
+
+			if ( ! empty( bp_nouveau()->members->button_args ) ) {
+				$buttons['private_message'] = wp_parse_args( array(
+					'position'  => 25,
+					'link_href' => esc_url( trailingslashit( bp_loggedin_user_domain() . bp_get_messages_slug() ) . '#compose?r=' . bp_core_get_username( $user_id ) ),
+				), bp_nouveau()->members->button_args );
+
+				unset( bp_nouveau()->members->button_args );
+			}
+		}
+
+		/**
+		 * Filter here to add your buttons, use the position argument to choose where to insert it.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array $buttons The list of buttons.
+		 * @param int   $user_id The displayed user ID.
+		 */
+		$buttons_group = apply_filters( 'bp_nouveau_get_member_header_buttons', $buttons, $user_id );
+
+		if ( empty( $buttons_group ) ) {
+			return $buttons;
+		}
+
+		bp_nouveau()->members->profile_buttons = new BP_Buttons_Group( $buttons_group );
+
+		$return = bp_nouveau()->members->profile_buttons->get();
+
+		if ( ! $return ) {
+			return array();
+		}
+
+		return $return;
+	}

bp-templates/bp-nouveau/buddypress/members/single/cover-image-header.php
@@ -35,14 +35,7 @@ do_action( 'bp_before_member_header' ); ?>
 				<h2 class="user-nicename">@<?php bp_displayed_user_mentionname(); ?></h2>
 			<?php endif; ?>
 
-			<div id="item-buttons"><?php
-
-				/**
-				 * Fires in the member header actions section.
-				 *
-				 * @since 1.2.6
-				 */
-				do_action( 'bp_member_header_actions' ); ?></div><!-- #item-buttons -->
+			<div id="item-buttons"><?php bp_nouveau_member_header_buttons(); ?></div><!-- #item-buttons -->
 
 			<span class="activity"><?php bp_last_activity( bp_displayed_user_id() ); ?></span>
 

bp-templates/bp-nouveau/buddypress/members/single/member-header.php
@@ -56,14 +56,7 @@ do_action( 'bp_before_member_header' ); ?>
 
 		<div id="item-buttons">
 
-			<?php
-
-			/**
-			 * Fires in the member header actions section.
-			 *
-			 * @since 1.2.6
-			 */
-			do_action( 'bp_member_header_actions' ); ?>
+			<?php bp_nouveau_member_header_buttons(); ?>
 
 		</div><!-- #item-buttons -->
 

bp-templates/bp-nouveau/includes/activity/loader.php
@@ -59,12 +59,6 @@ class BP_Nouveau_Activity {
 	 * @since 1.0.0
 	 */
 	private function setup_actions() {
-		if ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
-			if ( bp_activity_do_mentions() ) {
-				add_action( 'bp_member_header_actions', 'bp_send_public_message_button',  20 );
-			}
-		}
-
 		$ajax_actions = array(
 			array( 'activity_filter'                 => array( 'function' => 'bp_nouveau_ajax_object_template_loader',      'nopriv' => true ) ),
 			array( 'get_single_activity_content'     => array( 'function' => 'bp_nouveau_ajax_get_single_activity_content', 'nopriv' => true ) ),

bp-templates/bp-nouveau/includes/classes.php
@@ -80,7 +80,7 @@ class BP_Buttons_Group {
 
 			// Set the wrapper to default value if a class or an id for it is defined.
 			if ( ( ! empty( $r['wrapper_class'] ) || ! empty( $r['wrapper_id'] ) ) && false === $r['wrapper'] ) {
-				$r['wrapper'] = '';
+				$r['wrapper'] = 'div';
 			}
 
 			$this->group[ $r['id'] ] = $r;

bp-templates/bp-nouveau/includes/friends/loader.php
@@ -57,10 +57,6 @@ class BP_Nouveau_Friends {
 	 * @since 1.0.0
 	 */
 	private function setup_actions() {
-		if ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
-			add_action( 'bp_member_header_actions', 'bp_add_friend_button', 5 );
-		}
-
 		$ajax_actions = array(
 			array( 'friends_remove_friend'       => array( 'function' => 'bp_nouveau_ajax_addremove_friend', 'nopriv' => false ) ),
 			array( 'friends_add_friend'          => array( 'function' => 'bp_nouveau_ajax_addremove_friend', 'nopriv' => false ) ),

bp-templates/bp-nouveau/includes/functions.php
@@ -281,6 +281,11 @@ function bp_nouveau_get_forsaken_hooks() {
 			'message_type' => 'warning',
 			'message'      => __( 'The &#39;bp_activity_entry_meta&#39; action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the &#39;bp_nouveau_get_activity_entry_buttons&#39; filter instead', 'bp-nouveau' ),
 		),
+		'bp_member_header_actions' => array(
+			'hook_type'    => 'action',
+			'message_type' => 'warning',
+			'message'      => __( 'The &#39;bp_member_header_actions&#39; action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the &#39;bp_nouveau_get_member_header_buttons&#39; filter instead', 'bp-nouveau' ),
+		),
 	);
 }
 

bp-templates/bp-nouveau/includes/members/functions.php
@@ -119,3 +119,22 @@ function bp_nouveau_get_members_filters( $context = '' ) {
 
 	return bp_nouveau_parse_hooked_options( 'bp_members_directory_order_options', $filters );
 }
+
+/**
+ * Catch the arguments for buttons
+ *
+ * @since 1.0.0
+ *
+ * @param  array $buttons The arguments of the button that BuddyPress is about to create.
+ * @return array An empty array to stop the button creation process.
+ */
+function bp_nouveau_members_catch_button_args( $button = array() ) {
+	/**
+	 * Globalize the arguments so that we can use it
+	 * in bp_nouveau_get_member_header_buttons().
+	 */
+	bp_nouveau()->members->button_args = $button;
+
+	// return an empty array to stop the button creation process
+	return array();
+}

commit c95e66932c3478eacf55f4a309385e6c377b6268

Fix a regression introduced in ed58615003d33759c476edbfc245e3adb20c3331
We need to reset the group of buttons once displayed in the member header so that the friends loop can start with an empty global and set the right group of buttons.
I also renamed the $type to loop instead of directory as directory is a wrong information.

See #6

bp-templates/bp-nouveau/includes/members/template-tags.php
@@ -88,7 +88,17 @@ function bp_nouveau_after_members_directory_content() {
  * @since 1.0.0
  */
 function bp_nouveau_member_header_buttons() {
+	$bp_nouveau = bp_nouveau();
+
 	echo join( ' ', bp_nouveau_get_members_buttons() );
+
+	/**
+	 * On the member's header we need to reset the group button's global
+	 * once displayed as the friends component will use the member's loop
+	 */
+	if ( ! empty( $bp_nouveau->members->member_buttons ) ) {
+		unset( $bp_nouveau->members->member_buttons );
+	}
 
 	/**
 	 * Fires in the member header actions section.
@@ -103,7 +113,7 @@ function bp_nouveau_members_loop_buttons() {
 		return;
 	}
 
-	echo join( ' ', bp_nouveau_get_members_buttons( 'directory' ) );
+	echo join( ' ', bp_nouveau_get_members_buttons( 'loop' ) );
 
 	/**
 	 * Fires inside the members action HTML markup to display actions.
@@ -126,7 +136,7 @@ function bp_nouveau_members_loop_buttons() {
 
 		$buttons = array();
 
-		if ( 'directory' === $type ) {
+		if ( 'loop' === $type ) {
 			$user_id = bp_get_member_user_id();
 		} else {
 			$user_id = bp_displayed_user_id();
@@ -160,7 +170,7 @@ function bp_nouveau_members_loop_buttons() {
 		}
 
 		// Only add The public and private messages when not in a loop
-		if ( 'directory' !== $type ) {
+		if ( 'loop' !== $type ) {
 			if ( bp_is_active( 'activity' ) && bp_activity_do_mentions() ) {
 				/**
 				 * This filter workaround is waiting for a core adaptation