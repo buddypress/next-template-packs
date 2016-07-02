774ba012dc1868933716b32f51ffdf754d810fca
hugo <hugoashmore@nettropia.co.uk>
2016-07-02 21:14:44 +0100

    Update group & members single header common class token. Update common header style partial. Add provisional partial import file for specific group headers.

--- bp-templates/bp-nouveau/buddypress/groups/single/home.php
+++ bp-templates/bp-nouveau/buddypress/groups/single/home.php
@@ -14,7 +14,7 @@
 
 	<?php bp_nouveau_group_hook( 'before', 'home_content' ); ?>
 
-	<div id="item-header" role="complementary" data-bp-item-id="<?php bp_group_id(); ?>" data-bp-item-component="groups" class="groups-header single-group-header">
+	<div id="item-header" role="complementary" data-bp-item-id="<?php bp_group_id(); ?>" data-bp-item-component="groups" class="groups-header single-headers">
 
 		<?php bp_nouveau_group_header_template_part(); ?>
 
