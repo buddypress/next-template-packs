buddypress/next-template-packs

commit 6189e1c5207f559b7fc3afa7d1997d6b24a28333

Optimize the Group Members Template
- Create a new template groups/single/members-loop.php.
- Use the bp_nouveau_members_loop_buttons() in it to generate the Add/Remove friends buttons.
- Add 'bp_group_members_list_item_action' to the forsaken hooks. See #6
- Create a new template for the single items search forms in the common directory. Use a button instead of an input for the submit. And use a type search for the input to avoid doing it in buddypress.js
- Use this new template for the members single item search to check it is generic enough.
- Get rid of some unused Legacy functions (now i am sure we are not using them anymore)

bp-templates/bp-nouveau/buddypress/groups/single/members.php
@@ -2,160 +2,24 @@
 /**
  * BuddyPress - Groups Members
  *
- * @package BuddyPress
- * @subpackage bp-nouveau
+ * @since  1.0.0
+ *
+ * @package BP Nouveau
  */
 ?>
 
-<?php if ( empty( $_POST ) ) : ?>
-	<div class="item-list-tabs" id="subnav" role="navigation">
-		<ul>
-			<li class="groups-members-search" role="search">
-				<?php bp_directory_members_search_form(); ?>
-			</li>
+<div class="item-list-tabs" id="subnav" role="navigation">
+	<ul class="subnav clearfix">
 
-			<?php bp_groups_members_filter(); ?>
-			<?php
+		<?php bp_get_template_part( 'common/search/object-search-form' ); ?>
 
-			/**
-			 * Fires at the end of the group members search unordered list.
-			 *
-			 * Part of bp_groups_members_template_part().
-			 *
-			 * @since 1.5.0
-			 */
-			do_action( 'bp_members_directory_member_sub_types' ); ?>
+		<?php bp_groups_members_filter(); ?>
 
-		</ul>
-	</div>
+	</ul>
+</div>
 
-	<div id="members-group-list" class="group_members dir-list">
+<div id="members-group-list" class="group_members dir-list" data-bp-list="group_members">
 
-		<div id="bp-ajax-loader"><?php esc_html_e( 'Requesting the group members, please wait.', 'bp-nouveau' ) ;?></div>
-
-	<?php elseif ( bp_group_has_members( bp_ajax_querystring( 'group_members' ) ) ) : ?>
-
-		<?php
-
-		/**
-		 * Fires before the display of the group members content.
-		 *
-		 * @since 1.1.0
-		 */
-		do_action( 'bp_before_group_members_content' ); ?>
-
-		<div id="pag-top" class="pagination">
-
-			<div class="pag-count" id="member-count-top">
-
-				<?php bp_members_pagination_count(); ?>
-
-			</div>
-
-			<div class="pagination-links" id="member-pag-top">
-
-				<?php bp_members_pagination_links(); ?>
-
-			</div>
-
-		</div>
-
-		<?php
-
-		/**
-		 * Fires before the display of the group members list.
-		 *
-		 * @since 1.1.0
-		 */
-		do_action( 'bp_before_group_members_list' ); ?>
-
-		<ul id="member-list" class="item-list">
-
-			<?php while ( bp_group_members() ) : bp_group_the_member(); ?>
-
-				<li>
-					<a href="<?php bp_group_member_domain(); ?>">
-
-						<?php bp_group_member_avatar_thumb(); ?>
-
-					</a>
-
-					<h5><?php bp_group_member_link(); ?></h5>
-					<span class="activity"><?php bp_group_member_joined_since(); ?></span>
-
-					<?php
-
-					/**
-					 * Fires inside the listing of an individual group member listing item.
-					 *
-					 * @since 1.1.0
-					 */
-					do_action( 'bp_group_members_list_item' ); ?>
-
-					<?php if ( bp_is_active( 'friends' ) ) : ?>
-
-						<div class="action">
-
-							<?php bp_add_friend_button( bp_get_group_member_id(), bp_get_group_member_is_friend() ); ?>
-
-							<?php
-
-							/**
-							 * Fires inside the action section of an individual group member listing item.
-							 *
-							 * @since 1.1.0
-							 */
-							do_action( 'bp_group_members_list_item_action' ); ?>
-
-						</div>
-
-					<?php endif; ?>
-				</li>
-
-			<?php endwhile; ?>
-
-		</ul>
-
-		<?php
-
-		/**
-		 * Fires after the display of the group members list.
-		 *
-		 * @since 1.1.0
-		 */
-		do_action( 'bp_after_group_members_list' ); ?>
-
-		<div id="pag-bottom" class="pagination">
-
-			<div class="pag-count" id="member-count-bottom">
-
-				<?php bp_members_pagination_count(); ?>
-
-			</div>
-
-			<div class="pagination-links" id="member-pag-bottom">
-
-				<?php bp_members_pagination_links(); ?>
-
-			</div>
-
-		</div>
-
-		<?php
-
-		/**
-		 * Fires after the display of the group members content.
-		 *
-		 * @since 1.1.0
-		 */
-		do_action( 'bp_after_group_members_content' ); ?>
-
-	<?php else: ?>
-
-		<div id="message" class="info">
-			<p><?php _e( 'No members were found.', 'bp-nouveau' ); ?></p>
-		</div>
-
-	<?php endif; ?>
+	<div id="bp-ajax-loader"><?php esc_html_e( 'Requesting the group members, please wait.', 'bp-nouveau' ) ;?></div>
 
 </div><!-- .group_members.dir-list -->

bp-templates/bp-nouveau/includes/ajax.php
@@ -229,28 +229,6 @@ function bp_nouveau_ajax_object_template_loader() {
 /** @todo remove ??? **********************************************************/
 
 /**
- * Load messages template loop when searched on the private message page
- *
- * @since 1.6.0
- *
- * @return string Prints template loop for the Messages component.
- */
-function bp_legacy_theme_messages_template_loader() {
-	bp_get_template_part( 'members/single/messages/messages-loop' );
-	exit();
-}
-
-/**
- * Load group invitations loop to handle pagination requests sent via AJAX.
- *
- * @since 2.0.0
- */
-function bp_legacy_theme_invite_template_loader() {
-	bp_get_template_part( 'groups/single/invites-loop' );
-	exit();
-}
-
-/**
  * Load group membership requests loop to handle pagination requests sent via AJAX.
  *
  * @since 2.0.0

bp-templates/bp-nouveau/includes/functions.php
@@ -331,6 +331,11 @@ function bp_nouveau_get_forsaken_hooks() {
 			'message_type' => 'warning',
 			'message'      => __( 'The &#39;bp_directory_members_actions&#39; action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the &#39;bp_nouveau_get_members_buttons&#39; filter instead', 'bp-nouveau' ),
 		),
+		'bp_group_members_list_item_action' => array(
+			'hook_type'    => 'action',
+			'message_type' => 'warning',
+			'message'      => __( 'The &#39;bp_group_members_list_item_action&#39; action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the &#39;bp_nouveau_get_members_buttons&#39; filter instead', 'bp-nouveau' ),
+		),
 		'bp_group_header_meta' => array(
 			'hook_type'    => 'action',
 			'message_type' => 'warning',

bp-templates/bp-nouveau/js/buddypress.js
@@ -81,16 +81,6 @@ window.bp = window.bp || {};
 				// Transform text field into search field
 				$( '#buddypress li[role="search"] input[type=text]' ).prop( 'type', 'search' );
 
-				// The group members search misses some attributes and we can't use Theme Compat here
-				if ( $( '#buddypress li[role="search"]' ).hasClass( 'groups-members-search' )  && $( '#members-group-list' ).length ) {
-					$( '#buddypress li[role="search"]' ).attr( 'data-bp-search', 'group_members' );
-					$( '#members-group-list' ).attr( 'data-bp-list', 'group_members' );
-					$( '#group_members-order-select label').html(
-						$( '<span></span>' ).html( $( '#group_members-order-select label').html() ).addClass( 'bp-screen-reader-text' )
-					);
-					$( 'select#group_members-order-by' ).attr( 'data-bp-filter', 'group_members' );
-				}
-
 				// Add a title attribute and use an icon for the search submit button
 				search_title = $( '#buddypress li[role="search"] input[type=submit]' ).prop( 'value' );
 				$( '#buddypress li[role="search"] input[type=submit]' ).prop( 'title', search_title );
@@ -424,7 +414,7 @@ window.bp = window.bp || {};
 					}
 
 					if ( 'group_members' === object ) {
-						$.extend( queryData, { template: 'groups/single/members' } )
+						$.extend( queryData, { template: 'groups/single/members-loop' } )
 					}
 
 					// Populate the object list
@@ -578,7 +568,7 @@ window.bp = window.bp || {};
 
 			// On the Groups Members page, we specify a template
 			if ( 'group_members' === object ) {
-				template = 'groups/single/members';
+				template = 'groups/single/members-loop';
 			}
 
 			if ( 'friends' === object ) {
@@ -619,7 +609,7 @@ window.bp = window.bp || {};
 			}
 
 			if ( 'group_members' === object ) {
-				template = 'groups/single/members';
+				template = 'groups/single/members-loop';
 			}
 
 			self.objectRequest( {
@@ -638,7 +628,7 @@ window.bp = window.bp || {};
 		 * @return {[type]}       [description]
 		 */
 		showSearchSubmit: function( event ) {
-			$( event.delegateTarget ).find( 'input[type=submit]' ).show();
+			$( event.delegateTarget ).find( '[type=submit]' ).show();
 		},
 
 		/**
@@ -648,7 +638,7 @@ window.bp = window.bp || {};
 		 */
 		hideSearchSubmit: function( event ) {
 			if ( ! $( event.target ).val() ) {
-				$( event.delegateTarget ).find( 'input[type=submit]' ).hide();
+				$( event.delegateTarget ).find( '[type=submit]' ).hide();
 			}
 		},
 
@@ -661,7 +651,7 @@ window.bp = window.bp || {};
 			if ( ! $( event.target ).val() ) {
 				$( event.delegateTarget ).submit();
 			} else {
-				$( event.delegateTarget ).find( 'input[type=submit]' ).show();
+				$( event.delegateTarget ).find( '[type=submit]' ).show();
 			}
 		},
 

bp-templates/bp-nouveau/buddypress-functions.php
@@ -151,7 +151,6 @@ class BP_Nouveau extends BP_Theme_Compat {
 			 * @todo check if we still use these 2 actions, else remove it
 			 * and the corresponding functions
 			 */
-			'invite_filter'   => 'bp_legacy_theme_invite_template_loader',
 			'requests_filter' => 'bp_legacy_theme_requests_template_loader',
 		);
 

bp-templates/bp-nouveau/buddypress/common/search/object-search-form.php
@@ -1,0 +1,23 @@
+<?php
+/**
+* BP Object search form
+*
+* @since 1.0.0
+*
+* @package BP Nouveau
+*/
+?>
+
+<li role="search" data-bp-search="<?php bp_nouveau_search_object_data_attr() ;?>">
+
+	<form action="" method="get" id="<?php bp_nouveau_search_object_id( 'search-form' ) ;?>">
+		<label for="<?php bp_nouveau_search_object_id( 'search' ) ;?>">
+			<input type="search" name="<?php bp_nouveau_search_object_name( 'search' ) ;?>" id="<?php bp_nouveau_search_object_id( 'search' ) ;?>" placeholder="<?php bp_nouveau_search_object_default_text(); ?>">
+		</label>
+		<button type="submit" id="<?php bp_nouveau_search_object_id( 'search-submit' ) ;?>" name="<?php bp_nouveau_search_object_name( 'search_submit' ) ;?>">
+			<span class="dashicons dashicons-search"></span>
+			<span class="bp-screen-reader-text"><?php bp_nouveau_search_object_default_text(); ?></span>
+		</button>
+	</form>
+
+</li>

bp-templates/bp-nouveau/buddypress/groups/single/members-loop.php
@@ -1,0 +1,121 @@
+<?php
+/**
+ * Group Members Loop template
+ *
+ * @since  1.0.0
+ *
+ * @package BP Nouveau
+ */
+?>
+
+<?php if ( bp_group_has_members( bp_ajax_querystring( 'group_members' ) ) ) : ?>
+
+	<?php
+
+	/**
+	 * Fires before the display of the group members content.
+	 *
+	 * @since 1.1.0
+	 */
+	do_action( 'bp_before_group_members_content' ); ?>
+
+	<div id="pag-top" class="pagination">
+
+		<div class="pag-count" id="member-count-top">
+
+			<?php bp_members_pagination_count(); ?>
+
+		</div>
+
+		<div class="pagination-links" id="member-pag-top">
+
+			<?php bp_members_pagination_links(); ?>
+
+		</div>
+
+	</div>
+
+	<?php
+
+	/**
+	 * Fires before the display of the group members list.
+	 *
+	 * @since 1.1.0
+	 */
+	do_action( 'bp_before_group_members_list' ); ?>
+
+	<ul id="member-list" class="item-list">
+
+		<?php while ( bp_group_members() ) : bp_group_the_member(); ?>
+
+			<li data-bp-item-id="<?php echo esc_attr( bp_get_group_member_id() ); ?>" data-bp-item-component="members">
+				<a href="<?php bp_group_member_domain(); ?>">
+
+					<?php bp_group_member_avatar_thumb(); ?>
+
+				</a>
+
+				<h5><?php bp_group_member_link(); ?></h5>
+				<span class="activity"><?php bp_group_member_joined_since(); ?></span>
+
+				<?php
+
+				/**
+				 * Fires inside the listing of an individual group member listing item.
+				 *
+				 * @since 1.1.0
+				 */
+				do_action( 'bp_group_members_list_item' ); ?>
+
+				<div class="action">
+
+					<?php bp_nouveau_members_loop_buttons(); ?>
+
+				</div>
+			</li>
+
+		<?php endwhile; ?>
+
+	</ul>
+
+	<?php
+
+	/**
+	 * Fires after the display of the group members list.
+	 *
+	 * @since 1.1.0
+	 */
+	do_action( 'bp_after_group_members_list' ); ?>
+
+	<div id="pag-bottom" class="pagination">
+
+		<div class="pag-count" id="member-count-bottom">
+
+			<?php bp_members_pagination_count(); ?>
+
+		</div>
+
+		<div class="pagination-links" id="member-pag-bottom">
+
+			<?php bp_members_pagination_links(); ?>
+
+		</div>
+
+	</div>
+
+	<?php
+
+	/**
+	 * Fires after the display of the group members content.
+	 *
+	 * @since 1.1.0
+	 */
+	do_action( 'bp_after_group_members_content' ); ?>
+
+<?php else: ?>
+
+	<div id="message" class="info">
+		<p><?php _e( 'No members were found.', 'bp-nouveau' ); ?></p>
+	</div>
+
+<?php endif; ?>

bp-templates/bp-nouveau/buddypress/members/single/activity.php
@@ -20,9 +20,8 @@
 
 <div class="item-list-tabs no-ajax" id="subsubnav">
 	<ul>
-		<li class="member-search" role="search" data-bp-search="activity">
-			<?php bp_directory_activity_search_form(); ?>
-		</li>
+		<?php bp_get_template_part( 'common/search/object-search-form' ); ?>
+
 		<li id="activity-filter-select" class="last filter">
 			<label for="activity-filter-by"><span class="bp-screen-reader-text"><?php _e( 'Show:', 'bp-nouveau' ); ?></span></label>
 			<select id="activity-filter-by" data-bp-filter="activity">

commit e7648441bfc8fc7d81362db06cedffc64ceee801

Bring back the group members filters into the template pack
Fix a regression introduced in 6189e1c5207f559b7fc3afa7d1997d6b24a28333 to make sure the group members filters are triggering the ajax request.
Use the "bp_nouveau_filter_options()" to build the filters for group members, friends and members.
Add Two hooks to the forsaken ones see #6
Improve bp.Nouveau.ajax() function.

bp-templates/bp-nouveau/buddypress/groups/index.php
@@ -33,7 +33,7 @@
 	</div>
 
 	<div id="groups-dir-list" class="groups dir-list" data-bp-list="groups">
-		<div id="bp-ajax-loader">loading</div>
+		<div id="bp-ajax-loader"><?php esc_html_e( 'Loading the groups of the community, please wait.', 'bp-nouveau' ) ;?></div>
 	</div><!-- #groups-dir-list -->
 
 	<?php bp_nouveau_after_groups_directory_content(); ?>

bp-templates/bp-nouveau/buddypress/groups/single/members.php
@@ -13,7 +13,16 @@
 
 		<?php bp_nouveau_search_form(); ?>
 
-		<?php bp_groups_members_filter(); ?>
+		<li id="group_members-order-select" class="last filter">
+			<label for="group_members-order-by">
+				<span class="bp-screen-reader-text"><?php esc_html_e( 'Order By:', 'bp-nouveau' ); ?></span>
+			</label>
+			<select id="group_members-order-by" data-bp-filter="group_members">
+
+				<?php bp_nouveau_filter_options(); ?>
+
+			</select>
+		</li>
 
 	</ul>
 </div>

bp-templates/bp-nouveau/buddypress/members/single/friends.php
@@ -17,20 +17,12 @@
 
 			<li id="members-order-select" class="last filter">
 
-				<label for="members-friends"><?php _e( 'Order By:', 'bp-nouveau' ); ?></label>
-				<select id="members-friends">
-					<option value="active"><?php _e( 'Last Active', 'bp-nouveau' ); ?></option>
-					<option value="newest"><?php _e( 'Newest Registered', 'bp-nouveau' ); ?></option>
-					<option value="alphabetical"><?php _e( 'Alphabetical', 'bp-nouveau' ); ?></option>
+				<label for="members-friends">
+					<span class="bp-screen-reader-text"><?php esc_html_e( 'Order By:', 'bp-nouveau' ); ?></span>
+				</label>
+				<select id="members-friends" data-bp-filter="members">
 
-					<?php
-
-					/**
-					 * Fires inside the members friends order options select input.
-					 *
-					 * @since 2.0.0
-					 */
-					do_action( 'bp_member_friends_order_options' ); ?>
+					<?php bp_nouveau_filter_options(); ?>
 
 				</select>
 			</li>
@@ -48,9 +40,9 @@ switch ( bp_current_action() ) :
 
 		bp_nouveau_member_hook( 'before', 'friends_content' ); ?>
 
-		<div class="members friends">
+		<div class="members friends" data-bp-list="members">
 
-			<?php bp_get_template_part( 'members/members-loop' ) ?>
+			<div id="bp-ajax-loader"><?php esc_html_e( 'Loading your friends, please wait.', 'bp-nouveau' ) ;?></div>
 
 		</div><!-- .members.friends -->
 

bp-templates/bp-nouveau/includes/ajax.php
@@ -158,6 +158,11 @@ function bp_nouveau_ajax_object_template_loader() {
 		wp_send_json_error();
 	}
 
+	// Nonce check!
+	if ( empty( $_POST['nonce'] ) || ! wp_verify_nonce( $_POST['nonce'], 'bp_nouveau_' . $object ) ) {
+		wp_send_json_error();
+	}
+
 	$result = array();
 
 	if ( 'activity' === $object ) {

bp-templates/bp-nouveau/includes/functions.php
@@ -192,6 +192,16 @@ function bp_nouveau_get_forsaken_hooks() {
 			'message_type' => 'warning',
 			'message'      => __( 'The \'bp_members_directory_order_options\' action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the \'bp_nouveau_get_members_filters\' filter instead', 'bp-nouveau' ),
 		),
+		'bp_groups_members_order_options' => array(
+			'hook_type'    => 'action',
+			'message_type' => 'warning',
+			'message'      => __( 'The \'bp_groups_members_order_options\' action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the \'bp_nouveau_get_members_filters\' filter instead', 'bp-nouveau' ),
+		),
+		'bp_member_friends_order_options' => array(
+			'hook_type'    => 'action',
+			'message_type' => 'warning',
+			'message'      => __( 'The \'bp_member_friends_order_options\' action will soon be deprecated in the BP Nouveau template pack, we recommend you now use the \'bp_nouveau_get_members_filters\' filter instead', 'bp-nouveau' ),
+		),
 		'bp_activity_filter_options' => array(
 			'hook_type'    => 'action',
 			'message_type' => 'warning',
@@ -431,8 +441,15 @@ function bp_nouveau_get_component_filters( $context = '', $component = '' ) {
 	if ( empty( $component ) ) {
 		if ( 'directory' === $context || 'user' === $context ) {
 			$component = bp_current_component();
+
+			if ( 'friends' === $component ) {
+				$context   = 'friends';
+				$component = 'members';
+			}
 		} elseif ( 'group' === $context && bp_is_group_activity() ) {
 			$component = 'activity';
+		} elseif ( 'group' === $context && bp_is_group_members() ) {
+			$component = 'members';
 		}
 	}
 

bp-templates/bp-nouveau/includes/members/functions.php
@@ -94,24 +94,34 @@ function bp_nouveau_get_members_directory_nav_items() {
  * @return array the filters
  */
 function bp_nouveau_get_members_filters( $context = '' ) {
-	/**
-	 * The group context is managed in bp_groups_members_template_part()
-	 * This was done for backcompat reasons in BP Legacy.
-	 *
-	 * @todo We should probably bring back this template part into BP Nouveau
-	 * as we're building a brand new template pack. (Javascript would be more simple)
-	 */
-	if ( 'directory' !== $context ) {
-		return array();
-	}
+	if ( 'group' !== $context ) {
+		$filters = array(
+			'active' => __( 'Last Active', 'bp-nouveau' ),
+			'newest' => __( 'Newest Registered', 'bp-nouveau' ),
+		);
 
-	$filters = array(
-		'active' => __( 'Last Active', 'bp-nouveau' ),
-		'newest' => __( 'Newest Registered', 'bp-nouveau' ),
-	);
+		if ( bp_is_active( 'xprofile' ) ) {
+			$filters['alphabetical'] = __( 'Alphabetical', 'bp-nouveau' );
+		}
 
-	if ( bp_is_active( 'xprofile' ) ) {
+		$action = 'bp_members_directory_order_options';
+
+		if ( 'friends' === $context ) {
+			$action = 'bp_member_friends_order_options';
+		}
+
+	} else {
+		$filters = array(
+			'last_joined' => __( 'Newest', 'bp-nouveau' ),
+			'first_joined' => __( 'Oldest','bp-nouveau' ),
+		);
+
+		if ( bp_is_active( 'activity' ) ) {
+			$filters['group_activity'] = __( 'Group Activity', 'bp-nouveau' );
+		}
+
 		$filters['alphabetical'] = __( 'Alphabetical', 'bp-nouveau' );
+		$action = 'bp_groups_members_order_options';
 	}
 
 	/**
@@ -124,7 +134,7 @@ function bp_nouveau_get_members_filters( $context = '' ) {
 	 */
 	$filters = apply_filters( 'bp_nouveau_get_members_filters', $filters, $context );
 
-	return bp_nouveau_parse_hooked_options( 'bp_members_directory_order_options', $filters );
+	return bp_nouveau_parse_hooked_options( $action, $filters );
 }
 
 /**

bp-templates/bp-nouveau/js/buddypress.js
@@ -170,9 +170,9 @@ window.bp = window.bp || {};
 			}
 
 			// Extend posted data with stored data and object nonce
-			$.extend( post_data, bp.Nouveau.getStorage( 'bp-' + object ), { nonce: BP_Nouveau.nonces[object] } );
+			var postData = $.extend( {}, bp.Nouveau.getStorage( 'bp-' + object ), { nonce: BP_Nouveau.nonces[object] }, post_data );
 
-			this.ajax_request = $.post( BP_Nouveau.ajaxurl, post_data, 'json' );
+			this.ajax_request = $.post( BP_Nouveau.ajaxurl, postData, 'json' );
 
 			return this.ajax_request;
 		},