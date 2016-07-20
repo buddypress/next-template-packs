56b1089d0179e9468de957761c8994e402c6de31
Hugo - hnla <hugoashmore@tiscali.co.uk>
2016-07-20 17:30:31 +0100

    Update vertical navigation layout
    Add a new wrapping element to contain item-navs & item-body, this provides a styling hook for running backgrounds on for faux full height column.
    
    Adjust vert styles, nav backgrounds for selected and hover.

--- bp-templates/bp-nouveau/buddypress/members/single/home.php
+++ bp-templates/bp-nouveau/buddypress/members/single/home.php
@@ -19,17 +19,19 @@
 
 	</div><!-- #item-header -->
 
-	<?php if ( ! bp_nouveau_is_object_nav_in_sidebar() ) : ?>
+	<div class="bp-wrap">
+		<?php if ( ! bp_nouveau_is_object_nav_in_sidebar() ) : ?>
 
-		<?php bp_get_template_part( 'members/single/parts/item-nav' ); ?>
+			<?php bp_get_template_part( 'members/single/parts/item-nav' ); ?>
 
-	<?php endif; ?>
+		<?php endif; ?>
 
-	<div id="item-body" class="item-body">
+		<div id="item-body" class="item-body">
 
-		<?php bp_nouveau_member_template_part();?>
+			<?php bp_nouveau_member_template_part();?>
 
-	</div><!-- #item-body -->
+		</div><!-- #item-body -->
+	</div><!-- // .bp-wrap -->
 
 	<?php bp_nouveau_member_hook( 'after', 'home_content' ); ?>
 
