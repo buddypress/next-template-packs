1396151d8a066ea0a85286cc29b7e1a0667e1781
Hugo - hnla <hugoashmore@tiscali.co.uk>
2016-07-21 10:52:33 +0100

    Attempt to re-factor the single group members list loop to better match the general members loop. Allows us to flow the same styles via the generic bp-lists rulesets while still permitting group specific styling changes.

--- bp-templates/bp-nouveau/buddypress/groups/single/members-loop.php
+++ bp-templates/bp-nouveau/buddypress/groups/single/members-loop.php
@@ -16,23 +16,32 @@
 
 	<?php bp_nouveau_group_hook( 'before', 'members_list' ); ?>
 
-	<ul id="member-list" class="item-list bp-list">
+	<ul id="member-list" class="item-list members-list bp-list">
 
 		<?php while ( bp_group_members() ) : bp_group_the_member(); ?>
 
 			<li data-bp-item-id="<?php echo esc_attr( bp_get_group_member_id() ); ?>" data-bp-item-component="members">
-				<a href="<?php bp_group_member_domain(); ?>">
 
-					<?php bp_group_member_avatar_thumb(); ?>
+				<div class="item-avatar">
+					<a href="<?php bp_group_member_domain(); ?>">
+						<?php bp_group_member_avatar_thumb(); ?>
+					</a>
+				</div>
 
-				</a>
+				<div class="item">
 
-				<h5><?php bp_group_member_link(); ?></h5>
-				<span class="activity"><?php bp_group_member_joined_since(); ?></span>
+					<h2 class="list-title member-name"><?php bp_group_member_link(); ?></h2>
 
-				<?php bp_nouveau_group_hook( '', 'members_list_item' ); ?>
+					<p class="joined item-meta">
+						<?php bp_group_member_joined_since(); ?>
+					</p>
 
-				<?php bp_nouveau_members_loop_buttons(); ?>
+					<?php bp_nouveau_group_hook( '', 'members_list_item' ); ?>
+
+					<?php bp_nouveau_members_loop_buttons(); ?>
+
+				</div>
+
 			</li>
 
 		<?php endwhile; ?>
