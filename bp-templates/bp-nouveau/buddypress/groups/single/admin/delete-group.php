e58d27001fc8ab147e76bf2fb85cff2c4ffec2ff
Hugo - hnla <hugoashmore@tiscali.co.uk>
2016-07-21 14:09:14 +0100

    Add warning color var. Update delete group are you sure label for warning class, updates text color. optionally we could update message box icon color .

--- bp-templates/bp-nouveau/buddypress/groups/single/admin/delete-group.php
+++ bp-templates/bp-nouveau/buddypress/groups/single/admin/delete-group.php
@@ -10,4 +10,7 @@
 
 <?php bp_nouveau_user_feedback( 'group-delete-warning' ); ?>
 
-<label for="delete-group-understand"><input type="checkbox" name="delete-group-understand" id="delete-group-understand" value="1" onclick="if(this.checked) { document.getElementById('delete-group-button').disabled = ''; } else { document.getElementById('delete-group-button').disabled = 'disabled'; }" /> <?php _e( 'I understand the consequences of deleting this group.', 'bp-nouveau' ); ?></label>
+<label for="delete-group-understand" class="bp-label-text warn">
+	<input type="checkbox" name="delete-group-understand" id="delete-group-understand" value="1" onclick="if(this.checked) { document.getElementById('delete-group-button').disabled = ''; } else { document.getElementById('delete-group-button').disabled = 'disabled'; }" />
+	<?php _e( 'I understand the consequences of deleting this group.', 'bp-nouveau' ); ?>
+</label>
