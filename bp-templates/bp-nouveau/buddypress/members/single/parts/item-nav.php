661fab68107a59e630a31c2bc6cad37b50efc9c7
Hugo - hnla <hugoashmore@tiscali.co.uk>
2016-07-30 10:51:56 +0100

    Update Nav partial files. Provide a basic and better visual layout of main navs in mobile views and small tablets.
    Tweak colors/backgrounds & hover/focus rules.

--- bp-templates/bp-nouveau/buddypress/members/single/parts/item-nav.php
+++ bp-templates/bp-nouveau/buddypress/members/single/parts/item-nav.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-	<div class="<?php bp_nouveau_single_item_nav_classes(); ?> dark" id="object-nav" role="navigation">
+	<div class="<?php bp_nouveau_single_item_nav_classes(); ?>" id="object-nav" role="navigation">
 
 		<?php if ( bp_nouveau_has_nav( array( 'type' => 'primary' ) ) ) : ?>
 
