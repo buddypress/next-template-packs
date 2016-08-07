dc52fac11237bb2d251c11c7458c9cb512302e21
Hugo - hnla <hugoashmore@tiscali.co.uk>
2016-08-07 22:20:32 +0100

    Groups Single screens Manage members admin screen: * Commit re-factors members markup constructs adding DL lists for the three member types mods,admin, members. * Update styles for first minor pass.

--- bp-templates/bp-nouveau/buddypress/groups/single/admin/manage-members.php
+++ bp-templates/bp-nouveau/buddypress/groups/single/admin/manage-members.php
@@ -8,88 +8,101 @@
  */
 ?>
 
-<div class="bp-widget">
-	<h4><?php _e( 'Administrators', 'bp-nouveau' ); ?></h4>
+	<dl class="groups-manage-members-list">
+
+	<dt class="admin-section"><?php _e( 'Administrators', 'bp-nouveau' ); ?></dt>
 
 	<?php if ( bp_has_members( '&include='. bp_group_admin_ids() ) ) : ?>
+		<dd class="admin-listing">
+			<ul id="admins-list" class="item-list single-line">
 
-	<ul id="admins-list" class="item-list single-line">
+				<?php while ( bp_members() ) : bp_the_member(); ?>
+				<li>
 
-		<?php while ( bp_members() ) : bp_the_member(); ?>
-		<li>
-			<?php echo bp_core_fetch_avatar( array( 'item_id' => bp_get_member_user_id(), 'type' => 'thumb', 'width' => 30, 'height' => 30, 'alt' => sprintf( __( 'Profile picture of %s', 'bp-nouveau' ), bp_get_member_name() ) ) ); ?>
-			<h5>
-				<a href="<?php bp_member_permalink(); ?>"> <?php bp_member_name(); ?></a>
-				<?php if ( count( bp_group_admin_ids( false, 'array' ) ) > 1 ) : ?>
-				<span class="small">
-					<a class="button confirm admin-demote-to-member" href="<?php bp_group_member_demote_link( bp_get_member_user_id() ); ?>"><?php _e( 'Demote to Member', 'bp-nouveau' ); ?></a>
-				</span>
-				<?php endif; ?>
-			</h5>
-		</li>
-		<?php endwhile; ?>
+					<?php echo bp_core_fetch_avatar( array( 'item_id' => bp_get_member_user_id(), 'type' => 'thumb', 'width' => 30, 'height' => 30, 'alt' => sprintf( __( 'Profile picture of %s', 'bp-nouveau' ), bp_get_member_name() ) ) ); ?>
+					<p class="list-title member-name">
+						<a href="<?php bp_member_permalink(); ?>"> <?php bp_member_name(); ?></a>
+						<?php if ( count( bp_group_admin_ids( false, 'array' ) ) > 1 ) : ?>
 
-	</ul>
+						<p class="action">
+							<a class="button confirm admin-demote-to-member" href="<?php bp_group_member_demote_link( bp_get_member_user_id() ); ?>"><?php _e( 'Demote to Member', 'bp-nouveau' ); ?></a>
+						</p>
 
+						</li>
+
+						<?php endif; ?>
+					</p>
+
+				</li>
+				<?php endwhile; ?>
+
+			</ul>
+		</dd>
 	<?php endif; ?>
 
-</div>
+	<?php if ( bp_group_has_moderators() ) : ?>
 
-<?php if ( bp_group_has_moderators() ) : ?>
-	<div class="bp-widget">
-		<h4><?php _e( 'Moderators', 'bp-nouveau' ); ?></h4>
+		<dt class="moderator-section"><?php _e( 'Moderators', 'bp-nouveau' ); ?></dt>
 
+		<dd class="moderator-listing">
 		<?php if ( bp_has_members( '&include=' . bp_group_mod_ids() ) ) : ?>
 			<ul id="mods-list" class="item-list single-line">
 
 				<?php while ( bp_members() ) : bp_the_member(); ?>
 				<li>
+
 					<?php echo bp_core_fetch_avatar( array( 'item_id' => bp_get_member_user_id(), 'type' => 'thumb', 'width' => 30, 'height' => 30, 'alt' => sprintf( __( 'Profile picture of %s', 'bp-nouveau' ), bp_get_member_name() ) ) ); ?>
-					<h5>
+					<p class="list-title member-name">
 						<a href="<?php bp_member_permalink(); ?>"> <?php bp_member_name(); ?></a>
-						<span class="small">
-							<a href="<?php bp_group_member_promote_admin_link( array( 'user_id' => bp_get_member_user_id() ) ); ?>" class="button confirm mod-promote-to-admin" title="<?php esc_attr_e( 'Promote to Admin', 'bp-nouveau' ); ?>"><?php _e( 'Promote to Admin', 'bp-nouveau' ); ?></a>
-							<a class="button confirm mod-demote-to-member" href="<?php bp_group_member_demote_link( bp_get_member_user_id() ); ?>"><?php _e( 'Demote to Member', 'bp-nouveau' ); ?></a>
-						</span>
-					</h5>
+					</p>
+
+					<p class="action">
+						<a href="<?php bp_group_member_promote_admin_link( array( 'user_id' => bp_get_member_user_id() ) ); ?>" class="button confirm mod-promote-to-admin" title="<?php esc_attr_e( 'Promote to Admin', 'bp-nouveau' ); ?>"><?php _e( 'Promote to Admin', 'bp-nouveau' ); ?></a>
+						<a class="button confirm mod-demote-to-member" href="<?php bp_group_member_demote_link( bp_get_member_user_id() ); ?>"><?php _e( 'Demote to Member', 'bp-nouveau' ); ?></a>
+					</p>
+
 				</li>
+
 				<?php endwhile; ?>
 
 			</ul>
 
 		<?php endif; ?>
-	</div>
+	</dd>
 <?php endif ?>
 
 
-<div class="bp-widget">
-	<h4><?php esc_html_e( 'Members', 'bp-nouveau' ); ?></h4>
+	<dt class="gen-members-section"><?php esc_html_e( 'Members', 'bp-nouveau' ); ?></dt>
 
-	<?php if ( bp_group_has_members( 'per_page=15&exclude_banned=0' ) ) : ?>
+	<dd class="general-members-listing">
+		<?php if ( bp_group_has_members( 'per_page=15&exclude_banned=0' ) ) : ?>
 
-		<?php if ( bp_group_member_needs_pagination() ) : ?>
+			<?php if ( bp_group_member_needs_pagination() ) : ?>
 
-			<?php bp_nouveau_pagination( 'top' ) ; ?>
+				<?php bp_nouveau_pagination( 'top' ) ; ?>
 
-		<?php endif; ?>
+			<?php endif; ?>
 
-		<ul id="members-list" class="item-list single-line">
-			<?php while ( bp_group_members() ) : bp_group_the_member(); ?>
+			<ul id="members-list" class="item-list single-line">
+				<?php while ( bp_group_members() ) : bp_group_the_member(); ?>
 
-				<li class="<?php bp_group_member_css_class(); ?>">
-					<?php bp_group_member_avatar_mini(); ?>
+					<li class="<?php bp_group_member_css_class(); ?>">
+						<?php bp_group_member_avatar_mini(); ?>
 
-					<h5>
-						<?php bp_group_member_link(); ?>
+						<p class="list-title member-name">
+							<?php bp_group_member_link(); ?>
+							<span class="banned warn"><?php if ( bp_get_group_member_is_banned() ) _e( '(banned)', 'bp-nouveau' ); ?></span>
+						</p>
 
-						<?php if ( bp_get_group_member_is_banned() ) _e( '(banned)', 'bp-nouveau' ); ?>
+						<?php bp_nouveau_groups_manage_members_buttons( array('wrapper' => 'p') ) ; ?>
 
-						<?php bp_nouveau_groups_manage_members_buttons() ; ?>
-					</h5>
-				</li>
+					</li>
 
-			<?php endwhile; ?>
-		</ul>
+				<?php endwhile; ?>
+			</ul>
+	</dd>
+
+</dl>
 
 	<?php else:
 
@@ -97,4 +110,3 @@
 
 	endif; ?>
 
-</div>
