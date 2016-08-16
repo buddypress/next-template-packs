6a3539460486ed1217273f6d04d4eecbfcb17017
Hugo - hnla <hugoashmore@tiscali.co.uk>
2016-08-16 19:43:10 +0100

    Search form updates. Remove transitions from main nav hovers. Add common class to search form include markup.

--- bp-templates/bp-nouveau/buddypress/common/search/search-form.php
+++ bp-templates/bp-nouveau/buddypress/common/search/search-form.php
@@ -14,7 +14,7 @@
 		<label for="<?php bp_nouveau_search_selector_id( 'search' ) ;?>">
 			<input type="search" id="<?php bp_nouveau_search_selector_id( 'search' ) ;?>" name="<?php bp_nouveau_search_selector_name() ;?>" placeholder="<?php bp_nouveau_search_default_text(); ?>">
 		</label>
-		<button type="submit" id="<?php bp_nouveau_search_selector_id( 'search-submit' ) ;?>" name="<?php bp_nouveau_search_selector_name( 'search_submit' ) ;?>">
+		<button type="submit" id="<?php bp_nouveau_search_selector_id( 'search-submit' ) ;?>" class="nouveau-search-submit" name="<?php bp_nouveau_search_selector_name( 'search_submit' ) ;?>">
 			<span class="dashicons dashicons-search"></span>
 			<span class="bp-screen-reader-text"><?php bp_nouveau_search_default_text( '', false ); ?></span>
 		</button>
