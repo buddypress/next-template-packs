931db1f23d31d03fe5bda438ef9d7fa795f1dd80
Hugo Ashmore <hugo@nettropia.co.uk>
2016-10-18 20:00:38 +0100

    In /common/notices/template-notices.php remove html from bp_nouveau_template_message() as this function already generates markup causing nested paragraphs.

--- bp-templates/bp-nouveau/buddypress/common/notices/template-notices.php
+++ bp-templates/bp-nouveau/buddypress/common/notices/template-notices.php
@@ -9,7 +9,7 @@
 ?>
 <div class="<?php bp_nouveau_template_message_classes(); ?>">
 
-	<p><?php bp_nouveau_template_message(); ?></p>
+	<?php bp_nouveau_template_message(); ?>
 
 	<?php if ( bp_nouveau_has_dismiss_button() ) : ?>
 
