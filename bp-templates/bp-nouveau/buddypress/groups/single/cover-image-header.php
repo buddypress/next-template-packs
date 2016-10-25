752e3773f3c38b29f8f2c2037d461a5e3b63cc5f
Hugo Ashmore <hugo@nettropia.co.uk>
2016-10-25 15:00:43 +0100

    Re-factor group header templates
    * Update group meta calls to use array objects
    * Update general tokens attr to classes from ID's where necessary.
    
    See #87

--- bp-templates/bp-nouveau/buddypress/groups/single/cover-image-header.php
+++ bp-templates/bp-nouveau/buddypress/groups/single/cover-image-header.php
@@ -26,18 +26,25 @@
 
 			<?php bp_nouveau_group_header_buttons(); ?>
 
-			<span class="highlight"><?php bp_group_type(); ?></span>
-			<span class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_group_last_active( 0, array( 'relative' => false ) ) ); ?>"><?php printf( __( 'active %s', 'bp-nouveau' ), bp_get_group_last_active() ); ?></span>
+			<p class="highlight group-status"><strong><?php echo esc_html( bp_nouveau_group_meta()->status ); ?></strong></p>
+			<p class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_group_last_active( 0, array( 'relative' => false ) ) ); ?>"><?php printf( __( 'active %s', 'bp-nouveau' ), bp_get_group_last_active() ); ?></p>
+			<?php echo bp_nouveau_group_meta()->group_type_list; ?>
 
 			<?php bp_nouveau_group_hook( 'before', 'header_meta' ); ?>
 
-			<?php if ( bp_nouveau_group_has_meta() ): ?>
-				<div id="item-meta">
+			<?php if ( bp_nouveau_group_has_meta_extra() ): ?>
+				<div class="item-meta">
 
-					<?php bp_nouveau_group_meta(); ?>
+					<?php echo bp_nouveau_group_meta()->extra; ?>
 
-				</div><!-- #item-meta -->
+				</div><!-- .item-meta -->
 			<?php endif; ?>
+
+			<?php if( bp_nouveau_group_meta()->description ) { ?>
+				<div class="group-description">
+					<?php echo bp_nouveau_group_meta()->description; ?>
+				</div><!-- //.group_description -->
+			<?php	} ?>
 
 		</div><!-- #item-header-content -->
 
