8ce2ff5e5221220e89d3efff8baf6ed114f5023d
Hugo Ashmore <hugo@nettropia.co.uk>
2016-11-18 11:27:34 +0000

    Add user screens & group screens include parts for filters. #89

---
+++ bp-templates/bp-nouveau/buddypress/common/filters/groups-screens-filters.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* BP Nouveau User screens filters
+*
+* @since 1.0.0
+*
+* @package BP Nouveau
+*/
+?>
+<ul id="comp-filters" class="component-filters clearfix">
+		<li id="<?php bp_nouveau_filter_container_id(); ?>" class="last filter">
+			<label for="<?php bp_nouveau_filter_id(); ?>" class="bp-screen-reader-text">
+				<span ><?php bp_nouveau_filter_label(); ?></span>
+			</label>
+			<div class="select-wrap">
+				<select id="<?php bp_nouveau_filter_id(); ?>" data-bp-filter="<?php  bp_nouveau_filter_component(); ?>">
+
+					<?php bp_nouveau_filter_options(); ?>
+
+				</select>
+				<span class="select-arrow"></span>
+			</div>
+		</li>
+</ul>
