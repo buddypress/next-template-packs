a65a827bd4c1542895b496b496fb24736c2b236f
Hugo Ashmore <hugo@nettropia.co.uk>
2016-12-19 09:37:13 +0000

    Add an aria attr for search label.

--- bp-templates/bp-nouveau/buddypress/common/search/search-form.php
+++ bp-templates/bp-nouveau/buddypress/common/search/search-form.php
@@ -11,12 +11,12 @@
 <li class="<?php bp_nouveau_search_container_class(); ?> bp-search" role="search" data-bp-search="<?php bp_nouveau_search_object_data_attr() ;?>">
 
 	<form action="" method="get" id="<?php bp_nouveau_search_selector_id( 'search-form' ) ;?>">
-		<label for="<?php bp_nouveau_search_selector_id( 'search' ) ;?>">
+		<label for="<?php bp_nouveau_search_selector_id( 'search' ); ?>" aria-describedby="button-text">
 			<input type="search" id="<?php bp_nouveau_search_selector_id( 'search' ) ;?>" name="<?php bp_nouveau_search_selector_name() ;?>" placeholder="<?php bp_nouveau_search_default_text(); ?>">
 		</label>
 		<button type="submit" id="<?php bp_nouveau_search_selector_id( 'search-submit' ) ;?>" class="nouveau-search-submit" name="<?php bp_nouveau_search_selector_name( 'search_submit' ) ;?>">
 			<span class="dashicons dashicons-search"></span>
-			<span class="bp-screen-reader-text"><?php bp_nouveau_search_default_text( '', false ); ?></span>
+			<span id="button-text" class="bp-screen-reader-text"><?php bp_nouveau_search_default_text( '', false ); ?></span>
 		</button>
 	</form>
 
