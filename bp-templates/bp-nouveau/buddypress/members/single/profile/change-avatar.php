df294fa495b09f55dd2f0b07c16b594653a427a3
mercime <mercijavier@gmail.com>
2017-04-19 01:13:47 -0700

    Hide bp-icons from screen readers
    See #124

--- bp-templates/bp-nouveau/buddypress/members/single/profile/change-avatar.php
+++ bp-templates/bp-nouveau/buddypress/members/single/profile/change-avatar.php
@@ -16,7 +16,7 @@
 <?php if ( !(int)bp_get_option( 'bp-disable-avatar-uploads' ) ) : ?>
 
 	<p class="bp-feedback info">
-		<span class="bp-icon"></span>
+		<span class="bp-icon" aria-hidden="true"></span>
 		<span class="bp-help-text"><?php _e( 'Your profile photo will be used on your profile and throughout the site. If there is a <a href="http://gravatar.com">Gravatar</a> associated with your account email we will use that, or you can upload an image from your computer.', 'bp-nouveau' ); ?></span>
 	</p>
 
