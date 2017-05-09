003cae6071c08a91fad82dba8bc26b99be7a9319
Hugo Ashmore <hugo@nettropia.co.uk>
2017-05-09 11:14:58 +0100

    Update User Profile screens
    * Add class 'profile-edit to edit form.
    * Update table styles, padding/widths.
    * Add focus background for form inputs.
    * Reduce font-weights slightly.

--- bp-templates/bp-nouveau/buddypress/members/single/profile/edit.php
+++ bp-templates/bp-nouveau/buddypress/members/single/profile/edit.php
@@ -12,7 +12,7 @@ bp_nouveau_xprofile_hook( 'before', 'edit_content' );
 if ( bp_has_profile( 'profile_group_id=' . bp_get_current_profile_group_id() ) ) :
 	while ( bp_profile_groups() ) : bp_the_profile_group(); ?>
 
-<form action="<?php bp_the_profile_group_edit_form_action(); ?>" method="post" id="profile-edit-form" class="standard-form <?php bp_the_profile_group_slug(); ?>">
+<form action="<?php bp_the_profile_group_edit_form_action(); ?>" method="post" id="profile-edit-form" class="standard-form profile-edit <?php bp_the_profile_group_slug(); ?>">
 
 	<?php bp_nouveau_xprofile_hook( 'before', 'field_content' ); ?>
 
