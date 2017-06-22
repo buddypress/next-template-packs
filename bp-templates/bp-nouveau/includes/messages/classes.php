4a53451ae52f7973ff224d08a04fc8d6e8c52eba
Hugo Ashmore <hugo@nettropia.co.uk>
2017-06-22 11:17:50 +0100

    Update message class check.
    In messages/classes.php we need to retain a check for WP_List_tables, with class_exist check removed the Nouveau 'class extends' runs on the front end causing a fatal error.
    
    This commit adds back a check but for `is_admin()` & `current_user_can()`
    
    See https://github.com/buddypress/next-template-packs/commit/dcd386549980c99a1f176a581288be634fd2c427

--- bp-templates/bp-nouveau/includes/messages/classes.php
+++ bp-templates/bp-nouveau/includes/messages/classes.php
@@ -10,85 +10,89 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-class BP_Nouveau_Notices_List_Table extends WP_List_Table {
-	public function __construct( $args = array() ) {
-		parent::__construct( array(
-			'plural' => 'notices',
-			'singular' => 'notice',
-			'ajax' => true,
-			'screen' => isset( $args['screen'] ) ? $args['screen'] : null,
-		) );
-	}
+if ( is_admin() && current_user_can( 'activate_plugins' ) ) :
 
-	public function ajax_user_can() {
-		return bp_current_user_can( 'bp_moderate' );
-	}
-
-	public function prepare_items() {
-		$page     = $this->get_pagenum();
-		$per_page = $this->get_items_per_page( 'bp_nouveau_notices_per_page' );
-
-		$this->items = BP_Messages_Notice::get_notices( array(
-			'pag_num'  => $per_page,
-			'pag_page' => $page
-		) );
-
-		$this->set_pagination_args( array(
-			'total_items' => BP_Messages_Notice::get_total_notice_count(),
-			'per_page' => $per_page,
-		) );
-	}
-
-	public function get_columns() {
-		return apply_filters( 'bp_nouveau_notices_list_table_get_columns', array(
-			'subject'   => _x( 'Subject', 'Admin Notices column header', 'buddypress' ),
-			'message'   => _x( 'Content', 'Admin Notices column header', 'buddypress' ),
-			'date_sent' => _x( 'Created', 'Admin Notices column header', 'buddypress' ),
-		) );
-	}
-
-	public function single_row( $item ) {
-		$class = '';
-
-		if ( ! empty( $item->is_active ) ) {
-			$class = ' class="notice-active"';
+	class BP_Nouveau_Notices_List_Table extends WP_List_Table {
+		public function __construct( $args = array() ) {
+			parent::__construct( array(
+				'plural' => 'notices',
+				'singular' => 'notice',
+				'ajax' => true,
+				'screen' => isset( $args['screen'] ) ? $args['screen'] : null,
+			) );
 		}
 
-		echo "<tr{$class}>";
-		$this->single_row_columns( $item );
-		echo '</tr>';
-	}
-
-	public function column_subject( $item ) {
-		$actions = array(
-			'activate_deactivate' => '<a href="' . esc_url( wp_nonce_url( add_query_arg( array(
-				'page' => 'bp-notices',
-				'activate' => $item->id
-			), bp_get_admin_url( 'users.php' ) ) ), 'messages_activate_notice' ) . '" data-bp-notice-id="' . $item->id . '" data-bp-action="activate">' . esc_html__( 'Activate Notice', 'buddypress' ) . '</a>',
-			'delete' => '<a href="' . esc_url( wp_nonce_url( add_query_arg( array(
-				'page' => 'bp-notices',
-				'delete' => $item->id
-			), bp_get_admin_url( 'users.php' ) ) ), 'messages_delete_thread' ) . '" data-bp-notice-id="' . $item->id . '" data-bp-action="delete">' . esc_html__( 'Delete Notice', 'buddypress' ) . '</a>',
-		);
-
-		if ( ! empty( $item->is_active ) ) {
-			$actions['activate_deactivate'] = '<a href="' . esc_url( wp_nonce_url( add_query_arg( array(
-				'page' => 'bp-notices',
-				'deactivate' => $item->id
-			), bp_get_admin_url( 'users.php' ) ) ), 'messages_deactivate_notice' ) . '" data-bp-notice-id="' . $item->id . '" data-bp-action="deactivate">' . esc_html__( 'Deactivate Notice', 'buddypress' ) . '</a>';
+		public function ajax_user_can() {
+			return bp_current_user_can( 'bp_moderate' );
 		}
 
-		echo '<strong>' . apply_filters( 'bp_get_message_notice_subject', $item->subject ) . '</strong> ' . $this->row_actions( $actions );
-	}
+		public function prepare_items() {
+			$page     = $this->get_pagenum();
+			$per_page = $this->get_items_per_page( 'bp_nouveau_notices_per_page' );
 
-	public function column_message( $item ) {
-		echo apply_filters( 'bp_get_message_notice_text', $item->message );
-	}
+			$this->items = BP_Messages_Notice::get_notices( array(
+				'pag_num'  => $per_page,
+				'pag_page' => $page
+			) );
 
-	public function column_date_sent( $item ) {
-		echo apply_filters( 'bp_get_message_notice_post_date', bp_format_time( strtotime( $item->date_sent ) ) );
+			$this->set_pagination_args( array(
+				'total_items' => BP_Messages_Notice::get_total_notice_count(),
+				'per_page' => $per_page,
+			) );
+		}
+
+		public function get_columns() {
+			return apply_filters( 'bp_nouveau_notices_list_table_get_columns', array(
+				'subject'   => _x( 'Subject', 'Admin Notices column header', 'buddypress' ),
+				'message'   => _x( 'Content', 'Admin Notices column header', 'buddypress' ),
+				'date_sent' => _x( 'Created', 'Admin Notices column header', 'buddypress' ),
+			) );
+		}
+
+		public function single_row( $item ) {
+			$class = '';
+
+			if ( ! empty( $item->is_active ) ) {
+				$class = ' class="notice-active"';
+			}
+
+			echo "<tr{$class}>";
+			$this->single_row_columns( $item );
+			echo '</tr>';
+		}
+
+		public function column_subject( $item ) {
+			$actions = array(
+				'activate_deactivate' => '<a href="' . esc_url( wp_nonce_url( add_query_arg( array(
+					'page' => 'bp-notices',
+					'activate' => $item->id
+				), bp_get_admin_url( 'users.php' ) ) ), 'messages_activate_notice' ) . '" data-bp-notice-id="' . $item->id . '" data-bp-action="activate">' . esc_html__( 'Activate Notice', 'buddypress' ) . '</a>',
+				'delete' => '<a href="' . esc_url( wp_nonce_url( add_query_arg( array(
+					'page' => 'bp-notices',
+					'delete' => $item->id
+				), bp_get_admin_url( 'users.php' ) ) ), 'messages_delete_thread' ) . '" data-bp-notice-id="' . $item->id . '" data-bp-action="delete">' . esc_html__( 'Delete Notice', 'buddypress' ) . '</a>',
+			);
+
+			if ( ! empty( $item->is_active ) ) {
+				$actions['activate_deactivate'] = '<a href="' . esc_url( wp_nonce_url( add_query_arg( array(
+					'page' => 'bp-notices',
+					'deactivate' => $item->id
+				), bp_get_admin_url( 'users.php' ) ) ), 'messages_deactivate_notice' ) . '" data-bp-notice-id="' . $item->id . '" data-bp-action="deactivate">' . esc_html__( 'Deactivate Notice', 'buddypress' ) . '</a>';
+			}
+
+			echo '<strong>' . apply_filters( 'bp_get_message_notice_subject', $item->subject ) . '</strong> ' . $this->row_actions( $actions );
+		}
+
+		public function column_message( $item ) {
+			echo apply_filters( 'bp_get_message_notice_text', $item->message );
+		}
+
+		public function column_date_sent( $item ) {
+			echo apply_filters( 'bp_get_message_notice_post_date', bp_format_time( strtotime( $item->date_sent ) ) );
+		}
 	}
-}
+endif;
+
 
 class BP_Nouveau_Admin_Notices {
 
